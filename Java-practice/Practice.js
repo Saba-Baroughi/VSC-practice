b68e2a372827b05d9863a110a0e3cf9411ad74af
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-07-25 17:31:39 +0100

    exe.switch-case

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,13 +1,12 @@
-let browser = prompt("name any browser:", chrome);
-if (browser == "edge") {
-  alert("you have got the edge!");
-} else if (
-  browser == "chrome" ||
-  browser == "firefox" ||
-  browser == "safari" ||
-  browser == "opera"
-) {
-  alert("okay,we support these browsers, too");
-} else {
-  alert("we hope that this page looks ok!");
+let a = Number(prompt("a?", ""));
+switch (a) {
+  case 0:
+    alert(0);
+    break;
+  case 1:
+    alert(1);
+    break;
+  case 2:
+  case 3:
+    alert("2,3");
 }
