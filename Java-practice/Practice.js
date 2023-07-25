6c1e75ead9630d9f2f3ef8a1895c8566ca4c452f
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-07-25 12:53:12 +0100

    practice-java

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,2 +1,7 @@
-var userAge = prompt("Put your Age");
-alert(typeof userAge);
+let age = prompt("How old are you?");
+let gender = prompt("Which gender are you?", "male or female");
+if (age < 18 || gender === "female") {
+  alert("you are not allowed to enter");
+} else {
+  alert("Enter the panel");
+}
