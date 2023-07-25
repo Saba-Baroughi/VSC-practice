65eec875b516f6d1a8150229fbdba848ad6e1133
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-07-25 16:56:58 +0100

    average

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,7 +1,20 @@
-let age = prompt("How old are you?");
-let gender = prompt("Which gender are you?", "male or female");
-if (age < 18 || gender === "female") {
-  alert("you are not allowed to enter");
-} else {
-  alert("Enter the panel");
+let average = prompt("Enter your average:", 19);
+switch (average) {
+  case "18":
+  case "19":
+  case "20":
+    alert("A level");
+    break;
+  case "15":
+  case "16":
+  case "17":
+    alert("B level");
+    break;
+  case "12":
+  case "13":
+  case "14":
+    alert("C level");
+    break;
+  default:
+    alert("mashrut");
 }
