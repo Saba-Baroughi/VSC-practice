fcc5b70266de04542ea4186691ae2f84c4b84204
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-07-27 17:05:36 +0100

    Defualt(prameter)

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,6 +1,11 @@
-function average(num1, num2, num3) {
-  var sum = num1 + num2 + num3;
-  var result = sum / 3;
-  alert(result);
+function sumValue(num1, num2) {
+  if (num1 == undefined) {
+    num1 = 1;
+  }
+  if (num2 == undefined) {
+    num2 = 2;
+  }
+  var result = num1 + num2;
+  alert("result" + ":" + result);
 }
-average(2, 5, 4);
+sumValue(2);
