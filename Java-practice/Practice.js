20df67e98d347c95c6225a62cbb5f973041f199c
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-07-27 11:33:14 +0100

    prameter.prac

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,4 +1,19 @@
-function showmessage(from, text) {
-  alert(from + ":" + text);
+// let input = prompt("enter a number:");
+// if (input % 2 == 0) {
+//   function result() {
+//     alert("even");
+//   }
+// } else {
+//   function result() {
+//     alert("odd");
+//   }
+// }
+// result();
+function isOddOrEven(number) {
+  if (number % 2 == 0) {
+    alert(number + ":" + "is even");
+  } else {
+    alert(number + "is odd");
+  }
 }
-showmessage("saba", "hello");
+isOddOrEven(14);
