6bd3e6383e117153de76b8d19e52bc72e94f1d77
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-02 17:10:04 +0100

    Random,string

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,6 +1,2 @@
-var userName = prompt("Enter username:");
-if (userName.toLowerCase() == "ali") {
-  alert("Sign in");
-} else {
-  alert("Could not enter");
-}
+var firstRandomNumber = math.random() * 100000;
+console.log(firstRandomNumber);
