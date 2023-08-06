4da7a6d577ae0991cbd8344b21b1bdf591fd0038
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-06 20:56:56 +0100

    digit.counter.

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,7 +1,12 @@
 var userNumber = Number(prompt("Enter The Numnber", 0));
 var counter = 0;
-for (var i = 0; userNumber / 10 != 0; i++) {
-  counter++;
-  userNumber = Math.floor(userNumber / 10);
+if (userNumber == 0) {
+  counter = 1;
+} else {
+  for (var i = 0; userNumber / 10 != 0; i++) {
+    counter++;
+    userNumber = Math.floor(userNumber / 10);
+  }
 }
+
 alert(counter);
