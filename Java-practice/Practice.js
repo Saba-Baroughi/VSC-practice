bc3e7901bcd12513c58d5a4548d5a4e2df376be2
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-06 20:51:01 +0100

    counter.digits

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,7 +1,7 @@
 var userNumber = Number(prompt("Enter The Numnber", 0));
-var sum = 0;
+var counter = 0;
 for (var i = 0; userNumber / 10 != 0; i++) {
-  sum = sum + (userNumber % 10);
+  counter++;
   userNumber = Math.floor(userNumber / 10);
 }
-console.log("Sum of digits:", sum);
+alert(counter);
