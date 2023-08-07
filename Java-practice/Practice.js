d9409138a3b5031ad42bdbfe648b23897b14147f
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-07 16:45:59 +0100

    severalNumbersAverage

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,14 +1,13 @@
-var userFirstNumber = Number(prompt("Enter First Number?", 0));
-var userSecondNumber = Number(prompt("Enter Second Number?", 0));
-if (userFirstNumber % 2 === 0) {
-  while (userFirstNumber < userSecondNumber) {
-    console.log(userFirstNumber);
-    userFirstNumber += 2;
-  }
-} else {
-  userFirstNumber++;
-  while (userFirstNumber < userSecondNumber) {
-    console.log(userFirstNumber);
-    userFirstNumber += 2;
+var userNumber = 0;
+var sum = 0;
+var counter = 0;
+while (userNumber != -1) {
+  sum += userNumber;
+  userNumber = Number(
+    prompt("Enter the number\n\nIf you have not a number to add, Enter -1")
+  );
+  if (userNumber != -1) {
+    counter++;
   }
 }
+console.log("average" + " ", sum / counter);
