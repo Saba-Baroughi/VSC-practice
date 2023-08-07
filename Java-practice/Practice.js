6b4888bc1a283b7743b370de1e7970fb8c1f5389
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-07 11:12:47 +0100

    revised.digit.counter

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -2,6 +2,8 @@ var userNumber = Number(prompt("Enter The Numnber", 0));
 var counter = 0;
 if (userNumber == 0) {
   counter = 1;
+} else if (isNaN(userNumber)) {
+  alert("enter the number");
 } else {
   for (var i = 0; userNumber / 10 != 0; i++) {
     counter++;
