336c88c924ca0e44fec2c6331b4a3a68a3e9e8d0
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-08 19:07:30 +0100

    average.index

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,9 +1,15 @@
-var userFirst = Number(prompt("enter first number"));
-var userSecond = Number(prompt("enter second number"));
-var power = 1;
-var i = 0;
-do {
-  power = power * userFirst;
-  i++;
-} while (i < userSecond);
-alert("Power:" + power);
+var userNumbers = [];
+var userNumber = 0;
+var sum = 0;
+while (userNumber != -1) {
+  userNumber = Number(
+    prompt("Enter a number\n if you dont want a number put -1")
+  );
+  if (userNumber != -1) {
+    userNumbers.push(userNumber);
+  }
+}
+for (var i = 0; i < userNumbers.length; i++) {
+  sum = sum + userNumbers[i];
+}
+console.log("Average:", sum / userNumbers.length);
