9b16d7acefb77bb95fcf22f62c2bc7535098356e
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-08 16:36:50 +0100

    power.while

--- Java-practice/Practice.js
+++ Java-practice/Practice.js
@@ -1,7 +1,9 @@
 var userFirst = Number(prompt("enter first number"));
 var userSecond = Number(prompt("enter second number"));
 var power = 1;
-for (var i = 0; i < userSecond; i++) {
+var i = 0;
+while (i < userSecond) {
   power = power * userFirst;
+  i++;
 }
 alert("Power:" + power);
