9f003a57dc9b0d7838b02dd3815a424a0aed9423
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-17 21:20:03 +0100

    while.prac

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,8 +1,15 @@
-var users = ["ali", "memo", "hesi", "majid"];
-var newUsers = prompt("enter user name:");
-var isLogin = users.includes(newUsers);
-if (isLogin === true) {
-  console.log("welcome to...");
-} else {
-  console.log("open an account firstly");
+var userNumber = 0;
+var sum = 0;
+var counter = 0;
+
+while (userNumber != -1) {
+  sum += userNumber;
+
+  userNumber = Number(
+    prompt("Enter a number\n\n if yo dont have enter a number put -1")
+  );
+  if (userNumber != -1) {
+    counter++;
+  }
 }
+console.log(counter);
