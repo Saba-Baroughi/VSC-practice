13f749572921f47fcc27b19e272f864c1ad67f31
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-18 16:37:45 +0100

    prac.registration

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,17 +1,18 @@
-var userNumber = 0;
-var sum = 0;
-var counter = 0;
-
-var userNumbers = [];
-var userNumber = 0;
-while (userNumber != -1) {
-  sum += userNumber;
-  userNumber = Number(
-    prompt("enter a number \n \n if you do not want enter a number put -1")
-  );
-  if (userNumber != -1) {
-    userNumbers.push(userNumber);
-    counter++;
-  }
+var users = [
+  { id: 1, name: "Saba", family: "bonab", age: 35 },
+  { id: 2, name: "arash", family: "nasl", age: 35 },
+];
+var userName = prompt("enter you name:");
+var userFamily = prompt("enter your family");
+var userAge = Number(prompt("enter your age"));
+if (userName.length < 3 || userName.length > 10) {
+  alert("enter a valid name");
+} else if (userFamily.length > 15 || userFamily.length < 3) {
+  alert("enter the valid last name");
+} else if (isNaN(userAge) || userAge.length > 3) {
+  alert("enter a proper number:");
+} else {
+  var newUser = { id: 3, name: userName, family: userFamily, age: userAge };
+  users.push(newUser);
 }
-console.log(sum / counter);
+console.log(users);
