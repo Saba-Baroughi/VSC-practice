10e3109b4f1fb21ed3188609f63467cccfb4d1ea
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-18 11:23:13 +0100

    prac.average

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -5,11 +5,13 @@ var counter = 0;
 var userNumbers = [];
 var userNumber = 0;
 while (userNumber != -1) {
+  sum += userNumber;
   userNumber = Number(
     prompt("enter a number \n \n if you do not want enter a number put -1")
   );
   if (userNumber != -1) {
     userNumbers.push(userNumber);
+    counter++;
   }
 }
-console.log(userNumbers);
+console.log(sum / counter);
