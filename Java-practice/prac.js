fd545e6f9404b45507a786fae80385f6df06d84c
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-22 10:49:30 +0100

    for.each

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,4 +1,4 @@
-var users = ["saba", "ali", "baba", "ana"];
-users.forEach(function () {
-  console.log("username");
+var users = [1, 2, 3, 4];
+users.forEach(function (number) {
+  console.log(number * 3);
 });
