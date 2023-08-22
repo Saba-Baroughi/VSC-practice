7d5b81f642bd3724e4ad3eeaf69b435f1911186f
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-22 17:51:47 +0100

    shopping.list

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,4 +1,37 @@
-var users = [1, 2, 3, 4];
-users.forEach(function (number) {
-  console.log(number * 3);
+var allProducts = [
+  { id: 1, price: 100, name: "mobile" },
+  { id: 2, price: 10, name: "mug" },
+  { id: 3, price: 1, name: "pen" },
+  { id: 4, price: 12, name: "mouse" },
+  { id: 5, price: 11, name: "tablet" },
+];
+var UserBasket = [
+  { id: 3, price: 1, name: "pen" },
+  { id: 4, price: 12, name: "mouse" },
+  { id: 5, price: 11, name: "tablet" },
+];
+var userProduct = prompt("Enter any items:");
+var requestProduct;
+var isInShop = allProducts.some(function (products) {
+  if (products.name === userProduct) {
+    requestProduct = products;
+    return true;
+  }
 });
+
+if (isInShop == true) {
+  var newProduct = {
+    id: 7,
+    name: requestProduct.name,
+    price: requestProduct.price,
+  };
+  UserBasket.push(newProduct);
+  var sum = 0;
+  UserBasket.forEach(function (products) {
+    sum = sum + products.price;
+  });
+  console.log(UserBasket);
+  alert(sum);
+} else {
+  alert("Run out of the item");
+}
