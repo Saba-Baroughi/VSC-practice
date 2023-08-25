595e444762f8e64fc6d0784a9c7d0717e673027c
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-25 21:52:02 +0100

    every.prac

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,5 +1,17 @@
-var ages = [34, 35, 12];
-var isAll = ages.every(function (age) {
-  return age > 18;
+var dataBase = [
+  { id: 1, name: "saba", age: 32 },
+  { id: 2, name: "arash", age: 34 },
+  { id: 3, name: "Nasrin", age: 32 },
+  { id: 4, name: "Shahin", age: 12 },
+  { id: 5, name: "Mehri", age: 23 },
+  { id: 6, name: "Jafar", age: 67 },
+];
+var isInAge = dataBase.every(function (members) {
+  console.log(members);
+  return members.age > 18;
 });
-console.log(isAll);
+if (isInAge == true) {
+  alert("join to the conforence");
+} else {
+  alert("you are not allowed");
+}
