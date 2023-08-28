a3ca2d4e7c84fedb23d8654db513c3e6cf1706bc
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-28 09:54:36 +0100

    findIndex.prac

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,17 +1,6 @@
-var dataBase = [
-  { id: 1, name: "saba", age: 32 },
-  { id: 2, name: "arash", age: 34 },
-  { id: 3, name: "Nasrin", age: 12 },
-  { id: 4, name: "Shahin", age: 19 },
-  { id: 5, name: "Mehri", age: 23 },
-  { id: 6, name: "Jafar", age: 67 },
-];
-var isInAge = dataBase.every(function (members) {
-  console.log(members);
-  return members.age > 18;
+var users = ["saba", "memo", "arash", "heso", "majid"];
+var userIndex = users.findIndex(function (user) {
+  console.log(user);
+  return user == "arash";
 });
-if (isInAge == true) {
-  alert("join to the conforence");
-} else {
-  alert("you are not allowed");
-}
+console.log(userIndex);
