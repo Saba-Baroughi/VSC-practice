ebe5ffbc479579c755254489a57126ef11d84ce8
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-08-31 20:03:00 +0100

    shopping.filter

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,5 +1,20 @@
-var scores = [19, 20, 16, 18, 17];
-var scoresFilter = scores.filter(function (score) {
-  return score > 18;
+var userBasket = [
+  { id: 1, name: "labtop", price: 300 },
+  { id: 2, name: "phone", price: 250 },
+  { id: 3, name: "bed", price: 100 },
+  { id: 4, name: "pencil", price: 40 },
+  { id: 5, name: "mug", price: 3 },
+  { id: 6, name: "table", price: 80 },
+];
+var filterPrices = userBasket.filter(function (products) {
+  return products.price > 100;
 });
-console.log("scorefilter" + scoresFilter);
+var sumProducts = 0;
+var sumBasket = userBasket.map(function (sumItems) {
+  return (sumProducts = sumProducts + sumItems.price);
+});
+console.log(sumBasket[5]);
+var numbersPost = userBasket.length - filterPrices.length;
+var post = numbersPost * 2;
+var lastPrice = sumBasket[sumBasket.length - 1] + post;
+console.log(lastPrice);
