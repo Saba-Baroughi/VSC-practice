ee9a0668dd10dc18fc1e86980c6a6877fb0094c7
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-09-04 11:31:57 +0100

    word.reversed

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,18 +1,9 @@
-var userBasket = [
-  { id: 1, name: "labtop", price: 300 },
-  { id: 2, name: "phone", price: 250 },
-  { id: 3, name: "bed", price: 100 },
-  { id: 4, name: "pencil", price: 40 },
-  { id: 5, name: "mug", price: 3 },
-  { id: 6, name: "table", price: 80 },
-];
-var filterPrices = userBasket.filter(function (products) {
-  return products.price < 100;
-});
-var sumProducts = 0;
-var sumBasket = userBasket.forEach(function (sumItems) {
-  sumProducts = sumProducts + sumItems.price;
-});
-var post = filterPrices.length * 2;
-lastPrice = sumProducts + post;
-console.log(lastPrice);
+var word = prompt("Enter the word");
+var characterArray = word.split("");
+var reverseCharacters = characterArray.reverse();
+var reverseWord = reverseCharacters.join("");
+if (word === reverseWord) {
+  alert("they are reverse");
+} else {
+  alert("it is not ");
+}
