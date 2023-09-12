f26377f526911864027a890bc1da77cc1d2f0f90
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-09-12 11:01:22 +0100

    codeForGirls.Prac

--- Java-practice/prac.js
+++ Java-practice/prac.js
@@ -1,12 +1,10 @@
-var toDos = [
-  { id: 1, name: "study", status: "done" },
-  { id: 2, name: "cookin", status: "undone" },
-  { id: 3, name: "gym", status: "undone" },
-];
-var userRequest = prompt(
-  "Enter 1 if you add a do" +
-    "\n" +
-    "Enter 2 if you want remove a do" +
-    "\n" +
-    "Enter 3 if you want to start the task"
-);
+const PI = 3.14;
+var radius = prompt("Enter any radius:");
+var perimeter = 2 * PI * radius;
+console.log("The perimeter is equal to:" + " " + perimeter);
+alert("The perimeter is equal to:" + " " + perimeter);
+
+var FirstRandomNumber = Math.random() * 1000;
+console.log(FirstRandomNumber);
+var finalCaptchaCode = Math.floor(FirstRandomNumber);
+console.log("Your captcha number:", finalCaptchaCode);
