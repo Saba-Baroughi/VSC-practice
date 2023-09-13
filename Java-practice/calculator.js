876be4078eac1de31264920b58fb7229ec7dec70
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-09-13 17:57:25 +0100

    calculator.switch.case

--- Java-practice/calculator.js
+++ Java-practice/calculator.js
@@ -4,20 +4,35 @@ var num2 = +prompt("Enter The Second Number: ");
 var userOperator = prompt(
   "Enter The Operator: \n 1. + \n 2. - \n 3. * \n 4. / \n 5. **"
 );
-
-if (userOperator === "1") {
-  sum(num1, num2);
-} else if (userOperator === "2") {
-  tafrig(num1, num2);
-} else if (userOperator === "3") {
-  zarb(num1, num2);
-} else if (userOperator === "4") {
-  devide(num1, num2);
-} else if (userOperator === "5") {
-  power(num1, num2);
-} else {
-  alert("Enter proper number");
+switch (userOperator) {
+  case "1":
+    sum(num1, num2);
+    break;
+  case "2":
+    tafrig(num1, num2);
+    break;
+  case "3":
+    zarb(num1, num2);
+    break;
+  case "4":
+    devide(num1, num2);
+    break;
+  case "5":
+    power(num1, num2);
 }
+// if (userOperator === "1") {
+//   sum(num1, num2);
+// } else if (userOperator === "2") {
+//   tafrig(num1, num2);
+// } else if (userOperator === "3") {
+//   zarb(num1, num2);
+// } else if (userOperator === "4") {
+//   devide(num1, num2);
+// } else if (userOperator === "5") {
+//   power(num1, num2);
+// } else {
+//   alert("Enter proper number");
+// }
 
 function sum(num1, num2) {
   // return num1 + num2\
