abda8856f04c6e87478c8fbcfa9dda80f47e7a5f
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-10-04 13:37:01 +0100

    DOM-css

---
+++ Java-practice/Dom2.js
@@ -0,0 +1,15 @@
+function applyBackgroundColor() {
+  const selectedBgColor = document.getElementById("bg-color").value;
+  document.body.style.backgroundColor = selectedBgColor;
+}
+
+function applyFontFamily() {
+  const selectedFont = document.getElementById("font-family").value;
+  document.body.style.fontFamily = selectedFont;
+}
+
+function applyFontColor() {
+  //insert code here
+}
+
+document.getElementById("my_style").style.color = "#ababab";
