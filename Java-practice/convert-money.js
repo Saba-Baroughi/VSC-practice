938b6eb5a17abea1b09a1a568e60d5f5ce6ef7a0
Saba-Baroughi <saba.baroughi1111@gmail.com>
2023-10-04 12:44:00 +0100

    convert-money

---
+++ Java-practice/convert-money.js
@@ -0,0 +1,6 @@
+function convertCurrency() {
+  const gbpAmount = document.getElementById("gbp-amount").value;
+  const exchangeRate = 38.53;
+  const egyptianPounds = gbpAmount * exchangeRate;
+  document.getElementById("egp-amount").textContent = egyptianPounds;
+}
